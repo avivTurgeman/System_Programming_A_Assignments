e2e2bb824d6d8a29ab84c5a7dca6235cbde38df0
AvivTurgeman <aaviiv@gmail.com>
2022-12-05 20:15:47 +0200

    scanf("%c",selection) -> scanf("%s", selection)

--- main.c
+++ main.c
@@ -2,30 +2,30 @@
 #include "my_mat.h"
 
 
+// int matrix[10][10] = {{0, 3, 1, 0, 0, 2, 0, 0, 0, 0}, {3, 0, 1, 0, 0, 0, 0, 0, 0, 0}, {1, 1, 0, 0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 5, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 4, 1, 1}, {2, 0, 0, 0, 0, 0, 2, 0, 0, 0}, {0, 0, 0, 0, 0, 2, 0, 0, 0, 0}, {0, 0, 0, 5, 4, 0, 0, 0, 0, 2}, {0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0, 0, 2, 0, 0}};
 int matrix[10][10] = {0};
-
 int main(){
 
     char selection = '0';
     int executed = 0;
     while(1){
-        scanf("%c", &selection);
+        scanf("%s", &selection);
         if(selection == 'A'){
             A(matrix);
             executed = 0;
         }
-        if(selection == 'B'){
+        else if(selection == 'B'){
             B(matrix, executed);
             executed = 1;
         }
-        if(selection == 'C'){
+        else if(selection == 'C'){
             C(matrix,executed);
             executed = 1;
         }
-        if(selection == 'D'){
+        else if(selection == 'D'){
             break;
         }
-        if(selection != 'A' || selection != 'B' || selection != 'C' || selection != 'D'){
+        else if(selection != 'A' || selection != 'B' || selection != 'C' || selection != 'D'){
             break;
         }
     }
